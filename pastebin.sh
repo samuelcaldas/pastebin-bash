37cf4cf1811c83aaa98992aeb119926faa046b8f
Samuel Caldas <samuel.caldas@gmail.com>
2024-08-19 17:21:29 -0300

    feat: Update pastebin script to use environment variable for API key
    - Replaced hardcoded API key with a reference to the PASTEBIN_API_KEY environment variable for enhanced security and flexibility.
    - Added error handling to check if the PASTEBIN_API_KEY environment variable is set before executing the script.
    - This change allows users to securely manage their API keys without modifying the script directly.

--- pastebin.sh
+++ pastebin.sh
@@ -1,10 +1,13 @@
 pastebin () {
-  API_KEY='<your_api_key>'
-  if [ -z $1 ]
-  then
-    cat - | sed '1s/^/api_paste_code=/g' | sed 's/$/\%0A/g' | curl -d @- -d 'api_dev_key='"$API_KEY"'' -d 'api_option=paste' 'http://pastebin.com/api/api_post.php'
+  if [ -z "$PASTEBIN_API_KEY" ]; then
+    echo "Error: PASTEBIN_API_KEY environment variable is not set."
+    return 1
+  fi
+
+  if [ -z "$1" ]; then
+    cat - | sed '1s/^/api_paste_code=/g' | sed 's/$/\%0A/g' | curl -d @- -d "api_dev_key=$PASTEBIN_API_KEY" -d 'api_option=paste' 'http://pastebin.com/api/api_post.php'
   else
-    echo "$1" | sed '1s/^/api_paste_code=/g' | sed 's/$/\%0A/g' | curl -d @- -d 'api_dev_key='"$API_KEY"'' -d 'api_option=paste' 'http://pastebin.com/api/api_post.php'
+    echo "$1" | sed '1s/^/api_paste_code=/g' | sed 's/$/\%0A/g' | curl -d @- -d "api_dev_key=$PASTEBIN_API_KEY" -d 'api_option=paste' 'http://pastebin.com/api/api_post.php'
   fi
   printf '\n'
 }
